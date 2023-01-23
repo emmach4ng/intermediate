164faaa305f0be1505adf8641b4658e0434c4f8e
emmach4ng <114586807+emmach4ng@users.noreply.github.com>
2023-01-23 09:06:52 -0500

    Update NumberGuessGameJar.java

--- NumberGuessGame/src/NumberGuessGameJar.java
+++ NumberGuessGame/src/NumberGuessGameJar.java
@@ -11,22 +11,46 @@ import java.util.ArrayList;
 
 public class NumberGuessGameJar {
     public static void main(String args[]) throws Exception {
+        
+        System.out.println("Welcome to Number Guess Game. What range do you want the number to be in (must be integers)?");
+        Scanner sc = new Scanner(System.in);
+        for (;;) {
+            System.out.println("Enter the lowest integer of the range.");
+            if (sc.hasNextInt()) { //checks if the input is an integer
+                bottomRange = sc.nextInt();
+                break;
+            } else {
+                System.out.println("This is not an integer. Try again.");
+                sc.next(); // discarding the next input
+            }
+        }
+        for (;;) {
+            System.out.println("Enter the highest integer of the range.");
+            if (sc.hasNextInt()) { //checks if the input is an integer
+                topRange = sc.nextInt();
+                break;
+            } else {
+                System.out.println("This is not an integer. Try again.");
+                sc.next(); // discarding the next input
+            }
+        }
+        
         Random rand = new Random();
 
-        int number = rand.nextInt(100); //generates random integer to guess
+        int number = rand.nextInt(topRange - bottomRange + 1) + bottomRange; //generates random integer to guess
         int guesses = 0; //counts number of guesses
         ArrayList<Integer> numbersGuessed = new ArrayList<Integer>(); //stores previous guesses
 
         for (;;) { //loops until number is guessed
 
-            System.out.println("Guess a number between 1 and 100.");
+            System.out.println("Guess a number between " + bottomRange + " and " + topRange);
             Scanner input = new Scanner(System.in);
 
             if (input.hasNextInt()) { //checks if the input is an integer
                 int x = input.nextInt();
-                if (x > 100 || x < 1) { //checks if input is outside 1-100
-                    System.out.println("Hey! This is not an integer between 1-100. Try again.");
-                } else { //if input is inside 1-100...
+                if (x > topRange || x < bottomRange) { //checks if input is outside range
+                    System.out.println("Hey! This is not an integer in the range. Try again.");
+                } else { //if input is inside range...
                     if (x < number) { //if input is less than the number
 
                         if (numbersGuessed.contains(x)) {
