f2e47d761e5c0bfde696c06111b76909e0421ba2
emmach4ng <114586807+emmach4ng@users.noreply.github.com>
2023-04-26 20:11:35 -0400

    Add files via upload

---
+++ Superstore/EmployeeItem.java
@@ -0,0 +1,16 @@
+public class EmployeeItem extends Item {
+    
+    EmployeeItem(String name, int bottomPriceRange, int topPriceRange) {
+        super(name, bottomPriceRange, topPriceRange);
+    }
+
+    @Override
+    boolean isQuantityLimit() {
+        if (count > 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
